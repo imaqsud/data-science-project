aeaca561bd531a556050c80bf1f18ee9f218b484
Mohammed Maqsud Ansari <mohammed.ansari@sprinklr.com>
2017-10-02 13:15:16 +0530

    added PandasFunctions file

---
+++ pandaspkg/PandasFunctions.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+if True:
+    data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
+            'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions',
+                     'Lions', 'Lions'],
+            'wins': [11, 8, 10, 15, 11, 6, 10, 4],
+            'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
+    football = pd.DataFrame(data)
+    print(football)
+
+if True:
+    data = {'year': [2010, 2011, 2012, 2011, 2012, 2010, 2011, 2012],
+            'team': ['Bears', 'Bears', 'Bears', 'Packers', 'Packers', 'Lions',
+                     'Lions', 'Lions'],
+            'wins': [11, 8, 10, 15, 11, 6, 10, 4],
+            'losses': [5, 8, 6, 1, 5, 10, 6, 12]}
+    football = pd.DataFrame(data)
+    print(football.dtypes)
+    print("")
+    print(football.describe())
+    print("")
+    print(football.head())
+    print("")
+    print(football.tail())
